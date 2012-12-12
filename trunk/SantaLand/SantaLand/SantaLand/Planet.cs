8ef8c3e2f76a1b04144071f9b43760b045bde5e2
rasmus.eriksson88@gmail.com <rasmus.eriksson88@gmail.com@9fcaba57-fa2f-e43e-1624-e3b73d9a426c>
2012-12-12 10:33:30 +0000

    Lagt till fps-mätare, planet och mars-klasser och texturer

---
+++ trunk/SantaLand/SantaLand/SantaLand/Planet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SantaLand
+{
+    abstract class Planet 
+    {
+
+
+    }
+
+    static class PlanetHelper
+    {
+        struct HSL
+        {
+            public double h, s, l;
+        }
+
+        static HSL RGB2HSL(Color c1)
+        {
+            double themin, themax, delta;
+            HSL c2;
+            themin = Math.Min(c1.R, Math.Min(c1.G, c1.B));
+            themax = Math.Max(c1.R, Math.Max(c1.G, c1.B));
+            delta = themax - themin;
+            c2.l = (themin + themax) / 2;
+            c2.s = 0;
+            if (c2.l > 0 && c2.l < 1)
+                c2.s = delta / (c2.l < 0.5 ? (2 * c2.l) : (2 - 2 * c2.l));
+            c2.h = 0;
+            if (delta > 0)
+            {
+                if (themax == c1.R && themax != c1.G)
+                    c2.h += (c1.G - c1.B) / delta;
+                if (themax == c1.G && themax != c1.B)
+                    c2.h += (2 + (c1.B - c1.R) / delta);
+                if (themax == c1.B && themax != c1.R)
+                    c2.h += (4 + (c1.R - c1.G) / delta);
+                c2.h *= 60;
+            }
+            return (c2);
+        }
+    }
+}
