1eaa277b7053f5eebf66a9d801afc2014da9d897
robin.dorbell@gmail.com <robin.dorbell@gmail.com@9fcaba57-fa2f-e43e-1624-e3b73d9a426c>
2012-12-17 10:06:16 +0000

    Bättre struktur

---
+++ trunk/SantaLand/SantaLand/SantaLand/Planets/Sun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace SantaLand.Planets
+{
+    class Sun : Planet
+    {
+        public Sun(GraphicsDevice graphicsDevice, Vector3 lightDirection) 
+            : base(graphicsDevice, lightDirection)
+        {
+        }
+    }
+}
