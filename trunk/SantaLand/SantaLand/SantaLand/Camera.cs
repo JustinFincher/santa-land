3834f075b38aa88f269a9cab70c279137539f61c
rasmus.eriksson88@gmail.com <rasmus.eriksson88@gmail.com@9fcaba57-fa2f-e43e-1624-e3b73d9a426c>
2012-12-17 17:12:35 +0000

    lagt till OPAPRUTINTY och pillat kamrea i rumpan

---
+++ trunk/SantaLand/SantaLand/SantaLand/Camera.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SantaLand
+{
+    class Camera
+    {
+        Quaternion cameraRotation = Quaternion.Identity;
+        float cameraDelaySpeed = 0;
+        Vector3 thirdPersonReference = new Vector3(10, 0, 0);
+        bool activated = false;
+        Game1 game;
+
+        public Camera(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public void Activate()
+        {
+           activated = true;
+        }
+
+        public void Deactivate()
+        {
+            activated = false;
+        }
+
+        public void UpdateViewMatrix(Vehicle cameraTarget)
+        {
+            if (activated)
+            {
+                cameraRotation = Quaternion.Lerp(cameraRotation, cameraTarget.rotation, cameraDelaySpeed);
+                // Create a vector pointing the direction the camera is facing.
+                Vector3 transformedReference = Vector3.Transform(thirdPersonReference, cameraRotation);
+                // Calculate the position the camera is looking from.
+                Vector3 cameraPosition = transformedReference + cameraTarget.GetWorldPos();
+
+                float aspectRatio = game.GraphicsDevice.DisplayMode.AspectRatio;
+
+                Matrix.CreatePerspectiveFieldOfView(
+                    MathHelper.PiOver4,
+                    aspectRatio,
+                    0.1f,
+                    100000.0f,
+                    out game.projection);
+
+                game.view = Matrix.CreateLookAt(cameraPosition, cameraTarget.GetWorldPos(), Matrix.CreateFromQuaternion(cameraTarget.rotation).Up);
+            }
+        }
+    }
+}
---
+++ trunk/SantaLand/SantaLand/SantaLand/Vehicle.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace SantaLand
+{
+    class Vehicle : GameObject
+    {
+        protected Model model;
+        protected Game1 game;
+
+        private Planet planet;
+        private float longitude;
+        private float latitude;
+
+        public Vehicle(Game1 game, Model model, Planet planet)
+        {
+            this.model = model;
+            this.game = game;
+            this.planet = planet;
+            longitude = 0;
+            latitude = 0;
+            scale = Vector3.One * 0.01f;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            ProcessInput(gameTime);
+
+            float LAT = latitude * (float)Math.PI/180;
+            float LON = longitude * (float)Math.PI/180;
+            position.X = -planet.GetRadius() * (float)Math.Cos(LAT) * (float)Math.Cos(LON);
+            position.Y = planet.GetRadius() * (float)Math.Sin(LAT);
+            position.Z = planet.GetRadius() * (float)Math.Cos(LAT) * (float)Math.Sin(LON);
+            
+            Vector3 pointTo = Vector3.Normalize(Vector3.Zero - position);
+            Vector3 rotAxis = Vector3.Cross(Matrix.Identity.Down, pointTo);
+            pointTo.Normalize();
+            rotAxis.Normalize();
+            float rotAngle = (float)Math.Acos(Vector3.Dot(Matrix.Identity.Down, pointTo));
+            rotation = Quaternion.CreateFromAxisAngle(rotAxis, rotAngle);
+
+            base.Update(gameTime);
+        }
+
+        private void ProcessInput(GameTime gameTime)
+        {
+            KeyboardState keyState = Keyboard.GetState();
+
+            if (keyState.IsKeyDown(Keys.Up))
+                latitude -= 0.1f;
+            if (keyState.IsKeyDown(Keys.Down))
+                latitude += 0.1f;
+            if (keyState.IsKeyDown(Keys.Left))
+                longitude -= 0.1f;
+            if (keyState.IsKeyDown(Keys.Right))
+                longitude += 0.1f;
+        }
+
+        public override void Draw(BasicEffect basicEffect, Matrix parentWorld)
+        {
+            if (model != null)
+            {
+                Matrix[] transforms = new Matrix[model.Bones.Count];
+                float aspectRatio = game.GraphicsDevice.Viewport.Width / game.GraphicsDevice.Viewport.Height;
+                model.CopyAbsoluteBoneTransformsTo(transforms);
+
+                foreach (ModelMesh mesh in model.Meshes)
+                {
+                    foreach (BasicEffect effect in mesh.Effects)
+                    {
+                        effect.EnableDefaultLighting();
+
+                        effect.View = game.view;
+                        effect.Projection = game.projection;
+
+                        effect.World =
+                            Matrix.CreateScale(scale) *
+                            Matrix.CreateFromQuaternion(rotation) * 
+                            Matrix.CreateTranslation(position) *
+                            Matrix.CreateFromQuaternion(planet.rotation) *
+                            //parentWorld * 
+                            //transforms[mesh.ParentBone.Index] * 
+                            Matrix.CreateTranslation(planet.position);
+                    }
+                    mesh.Draw();
+                }
+            }
+        }
+
+        public Vector3 GetWorldPos()
+        {
+            return position + planet.position;
+        }
+    }
+}
